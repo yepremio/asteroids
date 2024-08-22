621e1dad0b1ac5197bad06e6c904012aef47a71d
yepremio <yeprem86@gmail.com>
2024-08-21 21:38:58 -0700

    added display settings

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    display.set_mode()
+    scren = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 if __name__ == "__main__":
     main()
 
