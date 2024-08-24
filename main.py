ab95a4e4de07f5cbdc089309eef5c8081c5df819
yepremio <yeprem86@gmail.com>
2024-08-23 21:29:44 -0700

    changes

--- main.py
+++ main.py
@@ -2,14 +2,30 @@ import pygame
 from constants import *
 
 def main():
+    # pygame setup
+
     pygame.init
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    display.set_mode()
-    scren = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_mode()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("black")
+
+        # RENDER GAME HERE
+        #
+        # flip() the display to put your work on screen/refresh
+        pygame.display.flip()
+        
+        # game loop sequencing
+
 if __name__ == "__main__":
     main()
 
