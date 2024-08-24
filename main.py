cfd34cb883109973e7b293353e7ec7af8454e55e
yepremio <yeprem86@gmail.com>
2024-08-24 10:00:08 -0700

    added game loop

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from constants import *
 def main():
     # pygame setup
 
-    pygame.init
-
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    pygame.display.set_mode()
+    
+    pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
 
     while running:
         for event in pygame.event.get():
@@ -24,7 +25,7 @@ def main():
         # flip() the display to put your work on screen/refresh
         pygame.display.flip()
         
-        # game loop sequencing
+        clock.tick(60) # limits FPS to 60
 
 if __name__ == "__main__":
     main()
