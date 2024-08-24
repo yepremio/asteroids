ed13961d6767e38530129db19c51d4b1a87b4587
yepremio <yeprem86@gmail.com>
2024-08-24 14:46:18 -0700

    completed ship movement

--- player.py
+++ player.py
@@ -22,11 +22,20 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
+        
+        if keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_DOWN]:
+            self.move(-dt)
+        if keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             self.rotate(dt)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-        
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
