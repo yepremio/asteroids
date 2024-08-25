2bb5da48d307dc39ed9576670d8fb221c3a5d8f9
yepremio <yeprem86@gmail.com>
2024-08-24 18:37:25 -0700

    added collision check

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def is_collision(self, obj):
+        distance = pygame.Vector2.distance_to(self.position, obj.position)
+        if distance <= self.radius + obj.radius:
+            return True
+        return False 
+
