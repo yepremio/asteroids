248dd38aad301349d41808b903116e9d7c01794a
yepremio <yeprem86@gmail.com>
2024-08-24 18:27:25 -0700

    fixes

--- asteroid.py
+++ asteroid.py
@@ -1,13 +1,14 @@
 import pygame
-from pygame.draw import line
+from constants import *
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position(), 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius,  2)
 
     def update(self, dt):
-        velocity * dt to it's position
-            move straight line
+        self.position += self.velocity * dt
+
